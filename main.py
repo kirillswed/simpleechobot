6f76e65d2be8746531328cf35be8c16caa641c94
Kirill <zxcnablok3@gmail.com>
2025-07-15 14:37:45 +0300

    a little changes

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 import os
+import dotenv
 
+dotenv.load_dotenv()
 API_TOKEN = os.getenv("token")
 
+dp = Dispatcher()
+
+
+
+@dp.message(F.text)
 async def echo_handler(message: Message):
     await message.answer(message.text)
 
+@dp.message(F.sticker)
+async def echo_handler(message: Message):
+    await message.answer(message.sticker)
+
+
+
 async def main():
     bot = Bot(API_TOKEN)
-    dp = Dispatcher()
-    dp.message.register(echo_handler)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
